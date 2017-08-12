2a4169e6f28031ce637fec59121b85fca1d9b05c
carlos salazar <carlosgsalazarf@gmail.com>
2017-08-12 16:51:36 -0500

    finished clock and class example

--- src/index.js
+++ src/index.js
@@ -55,57 +55,109 @@ import registerServiceWorker from './registerServiceWorker';
 //   )
 // }
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
+// function formatDate(date) {
+//   return date.toLocaleDateString();
+// }
 
-function Avatar(props) {
-  return (
-    <img className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  );
-}
+// function Avatar(props) {
+//   return (
+//     <img className="Avatar"
+//       src={props.user.avatarUrl}
+//       alt={props.user.name}
+//     />
+//   );
+// }
 
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
+// function UserInfo(props) {
+//   return (
+//     <div className="UserInfo">
+//       <Avatar user={props.user} />
+//       <div className="UserInfo-name">
+//         {props.user.name}
+//       </div>
+//     </div>
+//   )
+// }
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <UserInfo user={props.author} />
+//       <div className="Comment-text">
+//         {props.text}
+//       </div>
+//       <div className="Comment-date">
+//         {formatDate(props.date)}
+//       </div>
+//     </div>
+//   );
+// }
+
+// const comment = {
+//   date: new Date(),
+//   text: 'I hope you enjoy learning React!',
+//   author: {
+//     name: 'Hello Kitty',
+//     avatarUrl: 'http://placekitten.com/g/64/64'
+//   }
+// };
+
+// ReactDOM.render(
+//   <Comment author={comment.author} date={comment.date} text={comment.text} />,
+//   document.getElementById('root')
+// );
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+  }
+
+  componentDidMount() {
+    this.timerId =  setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+
+  tick() {
+    this.setState({date: new Date()})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <FormattedDate date={this.state.date} />
       </div>
+    );
+  }
+}
+
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
     </div>
   )
 }
 
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
+function tick() {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
   );
 }
 
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'http://placekitten.com/g/64/64'
-  }
-};
+setInterval(tick, 1000)
 
-ReactDOM.render(
-  <Comment author={comment.author} date={comment.date} text={comment.text} />,
-  document.getElementById('root')
-);
 
 
 registerServiceWorker()
