4a93a95581fc939600ff3cef98cd94466007bd80
carlos salazar <carlosgsalazarf@gmail.com>
2017-08-15 21:26:15 -0500

    added some event handling

--- src/index.js
+++ src/index.js
@@ -4,56 +4,90 @@ import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
-class Clock extends React.Component {
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       date: new Date(),
+//       counter: 0
+//     }
+//   }
+
+//   componentDidMount() {
+//     this.timerId =  setInterval(() => this.tick(), 1000)
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timerId)
+//   }
+
+//   tick() {
+//     this.setState({date: new Date()})
+//     this.setState( prevState => ({ counter: prevState.counter + 1 }))
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <FormattedDate date={this.state.date} />
+//         <h3> counter: {this.state.counter} seconds </h3>
+//       </div>
+//     );
+//   }
+// }
+
+// function FormattedDate(props) {
+//   return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+// }
+
+// function App(props) {
+//   return (
+//     <div>
+//       <Clock />
+//       <Clock />
+//       <Clock />
+//     </div>
+//   )
+// }
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+class Toggle extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      date: new Date(),
-      counter: 0
+      isToggled: false
     }
+
+    // make it so 'this' works in the callback
+
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  componentDidMount() {
-    this.timerId =  setInterval(() => this.tick(), 1000)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerId)
-  }
-
-  tick() {
-    this.setState({date: new Date()})
-    this.setState( prevState => ({ counter: prevState.counter + 1 }))
+  handleClick() {
+    this.setState( prevState => (
+      {isToggled: !prevState.isToggled}
+    ))
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <FormattedDate date={this.state.date} />
-        <h3> counter: {this.state.counter} seconds </h3>
-      </div>
-    );
+      <button onClick={this.handleClick} >
+        {this.state.isToggled ? 'On' : 'Off'}
+      </button>
+    )
   }
+
+
 }
 
-function FormattedDate(props) {
-  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
-}
-
-function App(props) {
-  return (
-    <div>
-      <Clock />
-      <Clock />
-      <Clock />
-    </div>
-  )
-}
 ReactDOM.render(
-  <App />,
+  <Toggle />, 
   document.getElementById('root')
-);
+)
 
 
 registerServiceWorker()
