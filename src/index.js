0a1e6261497bf815613183e6649e4bfde3a126ff
carlos salazar <carlosgsalazarf@gmail.com>
2017-08-16 21:24:55 -0500

    textarea

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ class Nameform extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {value: ''}
+    this.state = {value: 'the quick brown fox jumps over the lazy dog'}
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -28,7 +28,7 @@ class Nameform extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name: 
-          <input type="text" value={this.state.value} onChange={this.handleChange}  />
+          <textarea value={this.state.value} onChange={this.handleChange}  />
         </label>
         <input type="submit" value="Submit" />
       </form>
