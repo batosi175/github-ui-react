a775a305e90d002b863a3b1330183674bfddf43a
carlos salazar <carlosgsalazarf@gmail.com>
2017-08-16 21:01:30 -0500

    id as keys reused along sibling elements in both map functions."

--- src/index.js
+++ src/index.js
@@ -4,33 +4,42 @@ import './index.css'
 
 import registerServiceWorker from './registerServiceWorker'
 
-function ListItem(props) {
-  return (
-    <li>
-      { props.value }
-    </li>
-  )
-}
-
-
-function NumberList(props) {
-  const numbers = props.numbers
-  
-  const listItems = numbers.map( number => 
-    // <li key={ number.toString() }>{ number }</li>
-    <ListItem key={ number.toString() } value={ number }/>
-  )
-  return (
+function Blog(props) {
+  const sidebar = (
     <ul>
-      { listItems }
+      {
+        props.posts.map(post => 
+          <li key={post.id}>
+            {post.title}
+          </li>
+        )
+      }
     </ul>
   )
+
+  const content = props.posts.map(post => 
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  )
+
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  )
 }
 
-const numbers = [1,2,3,4,5]
+const posts = [
+  { id: 1, title: 'hello World', content: 'welcome to learning react' }, 
+  { id: 2, title: 'hacker news', content: 'get all your leet hacker stuffs'}
+]
 
 ReactDOM.render(
-  <NumberList numbers={numbers}/>,
+  <Blog posts={posts}/>,
   document.getElementById('root')
 )
 
