231c25b3ea52a2054fe3b8f0335d7c94a0b39216
carlos salazar <carlosgsalazarf@gmail.com>
2017-08-20 13:16:21 -0500

    static app finished up

--- src/index.js
+++ src/index.js
@@ -3,87 +3,103 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 import registerServiceWorker from './registerServiceWorker'
-
-function FancyBorder(props) {
-  const color = props.color
-  return (
-    <div className={'FancyBorder FancyBorder-' + props.color } style={{border: `solid 5px ${color}`}} >
-      {props.children}
-    </div>  
-  )
-}
-
-function Dialog(props){
-  return (
-    <FancyBorder color='red' >
-      <h1 className='Dialog-title'>{props.title}</h1>
-      <p className='Dialog-message'>
-        {props.message}
-      </p>
-      {props.children}
-    </FancyBorder>
-  )
-}
-
-function WelcomeDialog(){
-  return (
-    <Dialog 
-      title="welcome to react" 
-      message="Thank you for visting our spacecraft!!"
-    />
-  )
-}
-
-class SignUpDialog extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {login: '', submitted: false}
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSignUp = this.handleSignUp.bind(this)
-  }
-
-  handleChange(e) {
-    this.setState({login: e.target.value})
-  }
-
-  handleSignUp() {
-    // alert(`Welcome Aboard ${this.state.login}!!`)
-    this.setState({submitted: true});
-  }
-
+// will render the category for a product
+class ProductCategoryRow extends React.Component {
   render() {
-    const submitted = this.state.submitted
-    const login = this.state.login
-    if (submitted) {
-      return (
-        <Dialog title={`Mars Exploration Program: Welcome Aboard ${login}`}
-              message="Shall we reclassify you?">
-        <input value={this.state.login}
-                onChange={this.handleChange} />
-        <button onClick={this.handleSignUp}>
-          Change my Name
-        </button>
-      </Dialog>
-      )
-    } else {
-      return (
-        <Dialog title="Mars Exploration Program"
-                message="How should we refer to you?">
-          <input value={this.state.login}
-                 onChange={this.handleChange} />
-          <button onClick={this.handleSignUp}>
-            Sign Me Up!
-          </button>
-        </Dialog>
-      )
-    }
+    return(
+      <tr><th colSpan='2'>{this.props.category}</th></tr>
+    )
   }
 }
+
+// will render product
+class ProductRow extends React.Component {
+  render () {
+    var name = this.props.product.stocked ? 
+      this.props.product.name : 
+      <span style={{color: 'red'}}>
+        {this.props.product.name}
+      </span>
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    )
+
+  }
+}
+
+// will contain products row and and product category row
+class ProductTable extends React.Component {
+  render() {
+    var rows = []
+    var lastCategory = null
+    this.props.products.forEach(product => {
+      if (product.category !== lastCategory){
+        rows.push(
+          <ProductCategoryRow category={product.category} key={product.category} />
+        )
+      }
+      rows.push(
+        <ProductRow product={product} key={product.name} />
+      )
+      lastCategory = product.category
+    })
+    return (
+      <table>
+        <thead>
+          <tr>
+          <th>Name:</th>
+          <th>Price:</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
+  }
+}
+
+// will contain search feature
+class SearchBar extends React.Component{
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {''}
+          Only show products in stock
+        </p>
+      </form>
+    )
+  }
+}
+
+// will contain searchbar and products table
+class FilterableProductTable extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    )
+  }
+}
+
+const products = [
+  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+]
 
 ReactDOM.render(
-  <SignUpDialog />,
-  document.getElementById('root')
-
+  <FilterableProductTable products={products} />,
+  document.getElementById('container')
 )
 
 registerServiceWorker()
