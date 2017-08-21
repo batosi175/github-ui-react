96de05e9f10ed0fb5b6ad5fe517a8844784c46f8
carlos salazar <carlosgsalazarf@gmail.com>
2017-08-20 22:02:59 -0500

    started creating teh basic structure for the application

--- src/index.js
+++ src/index.js
@@ -3,153 +3,70 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 import registerServiceWorker from './registerServiceWorker'
-// will render the category for a product
-class ProductCategoryRow extends React.Component {
-  render() {
-    return(
-      <tr><th colSpan='2'>{this.props.category}</th></tr>
-    )
+// application meant to be a simple orgs listing for and details view for github components
+
+class OrgList extends React.Component{
+  constructor(props) {
+    super(props)
+    this.orgClicked = this.orgClicked.bind(this)
   }
-}
 
-// will render product
-class ProductRow extends React.Component {
-  render () {
-    var name = this.props.product.stocked ? 
-      this.props.product.name : 
-      <span style={{color: 'red'}}>
-        {this.props.product.name}
-      </span>
-    return (
-      <tr>
-        <td>{name}</td>
-        <td>{this.props.product.price}</td>
-      </tr>
-    )
-
+  orgClicked(e) {
+    const {id} = e.target;
+    this.props.onOrgClicked(id)
   }
-}
 
-// will contain products row and and product category row
-class ProductTable extends React.Component {
   render() {
-    const inStockOnly = this.props.inStockOnly
-    var rows = []
-    var lastCategory = null
-    this.props.products.forEach(product => {
-
-      // check if filter text fails, stockonly and not in stock 
-
-      if (product.name.indexOf(this.props.filterText) === -1 || (this.props.inStockOnly && !product.stocked)) {
-        return
-      }
-
-      // check if it is a category value
-      if (product.category !== lastCategory){
-        rows.push(
-          <ProductCategoryRow category={product.category} key={product.category} />
-        )
-      }
-
-      // push it as product if other special checks fail
-      rows.push(
-        <ProductRow product={product} key={product.name} />
+    var orgs = this.props.orgs.map(org => {
+      return (
+      <div key={org.id} id={org.id} onClick={this.orgClicked} className={'flex-item'} >
+        {org.id}
+      </div>
       )
-      lastCategory = product.category
     })
     return (
-      <table>
-        <thead>
-          <tr>
-          <th>Name:</th>
-          <th>Price:</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-    )
-  }
-}
-
-// will contain search feature
-class SearchBar extends React.Component{
-  constructor(props) {
-    super(props)
-    this.handleFilterTextInputChanged = this.handleFilterTextInputChanged.bind(this)
-    this.handleStockOnlyInputChanged = this.handleStockOnlyInputChanged.bind(this)
-  }
-
-  handleFilterTextInputChanged(e) {
-    this.props.onFilterTextInput(e.target.value)
-  }
-
-  handleStockOnlyInputChanged(e) {
-    this.props.onStockOnlyInput(e.target.checked)
-  }
-
-  render() {
-    const filterText = this.props.filterText
-    const inStockOnly = this.props.inStockOnly
-    return (
-      <form>
-        <input type="text" placeholder="Search..." value={filterText} onChange={this.handleFilterTextInputChanged} />
-        <p>
-          <input type="checkbox" checked={inStockOnly} onChange={this.handleStockOnlyInputChanged} />
-          {''}
-          Only show products in stock
-        </p>
-      </form>
-    )
-  }
-}
-
-// will contain searchbar and products table
-class FilterableProductTable extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state={filterText: '', inStockOnly: false}
-    this.handleFilterTextInput = this.handleFilterTextInput.bind(this)
-    this.handleStockOnlyInput = this.handleStockOnlyInput.bind(this)
-  }
-
-  handleFilterTextInput(value) {
-    this.setState({filterText: value})
-  }
-
-  handleStockOnlyInput(value) {
-    this.setState({inStockOnly: value})
-  }
-
-  render() {
-    return (
-      <div>
-        <SearchBar
-          filterText={this.state.filterText}
-          inStockOnly={this.state.inStockOnly}
-          onFilterTextInput={this.handleFilterTextInput}
-          onStockOnlyInput={this.handleStockOnlyInput}
-         />
-        <ProductTable 
-        products={this.props.products}
-        filterText={this.state.filterText}
-        inStockOnly={this.state.inStockOnly}
-        />
+      <div className={'flex-container'}> 
+        {orgs}
       </div>
     )
   }
 }
 
-const products = [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+class OrgsAndDetails extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {currentOrg: ''}
+    this.handleOrgClicked = this.handleOrgClicked.bind(this)
+  }
+
+  handleOrgClicked(id){
+    this.setState({currentOrg: id})
+  }
+
+  render() {
+    var orgs = this.props.orgs
+    return (
+      <div>
+        <OrgList orgs={orgs} onOrgClicked={this.handleOrgClicked} />
+        <div className={'org-details'}>{this.state.currentOrg}</div>
+      </div>
+    )
+  }
+
+}
+
+const orgs = [
+  {id: 'blizzard'},
+  {id: 'facebook'},
+  {id: 'netflix'},
+  {id: 'yahoo'},
+  {id: 'emberjs'}
 ]
 
 ReactDOM.render(
-  <FilterableProductTable products={products} />,
+  <OrgsAndDetails
+    orgs={orgs}
+  />,
   document.getElementById('container')
 )
 
